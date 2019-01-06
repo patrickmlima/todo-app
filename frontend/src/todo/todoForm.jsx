ae2295ce7535fc7511753f9e07a1067eb0a340f9
Patrick Lima <patrickm.lima1@gmail.com>
2019-01-06 19:39:26 -0400

    Handling add and delete events for todo items [front-end]

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,11 +6,15 @@ import IconButton from '../template/iconButton'
 export default props => (
   <div role="form" className="todoForm row">
     <Grid cols="12 9 10">
-      <input id="description" className="form-control" placeholder="Adicione uma tarefa" />
+      <input id="description" className="form-control"
+        placeholder="Adicione uma tarefa"
+        onChange={props.handleChange}
+        value={props.description} />
     </Grid>
 
     <Grid cols="12 3 2">
-      <IconButton style="primary" icon="plus"></IconButton>
+      <IconButton style="primary" icon="plus"
+        onClick={props.handleAdd}></IconButton>
     </Grid>
   </div>
 )
